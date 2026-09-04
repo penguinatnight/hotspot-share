8f94daf710fd76dc843f783e386c94c3d592d234
penguinatnight <penguinatnight1@gmail.com>
2026-09-04 12:57:36 +0300

    release(v2.0.10): add launcher frontend health check, zombie server killer, and package-bundled web assets

--- setup.py
+++ setup.py
@@ -5,11 +5,13 @@ web_files = [str(p) for p in Path("web").glob("*") if p.is_file()]
 
 setup(
     name="hotspot-share",
-    version="2.0.9",
+    version="2.0.10",
     description="High-Speed Local Wi-Fi File Sharing & Multimodal Clipboard Sync",
     author="penguinatnight", author_email="penguinatnight1@gmail.com", url="https://github.com/penguinatnight/hotspot-share",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    package_data={"hotspot_share": ["web/*"]},
+    include_package_data=True,
     data_files=[
         ("share/hotspot-share/web", web_files),
     ],
